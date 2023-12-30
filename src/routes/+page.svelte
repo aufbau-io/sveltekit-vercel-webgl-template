9eb88c75c429a047ca57191697c3d766048693d2
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-12-30 17:34:15 +0000

    render from static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script>
-	import aufbau from "$lib/images/aufbau.svg";
 </script>
 
-<img src={aufbau} alt="aufbau logo" />
+<img src="/aufbau.svg" alt="aufbau logo" />
 
 <style>
 	img {
 		width: 80px;
-		height: 80px;
 		object-fit: contain;
 	}
 </style>
